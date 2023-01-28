2e52f964683a090fb698df057b0026f9cd4dae55
melody <35177464+HexCodeFFF@users.noreply.github.com>
2023-01-27 21:33:39 -0600

    icon and 1.0

--- src/main/java/net/reticivis/infineffect/mixin/InfineffectMixin.java
+++ src/main/java/net/reticivis/infineffect/mixin/InfineffectMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(net.minecraft.entity.effect.StatusEffectUtil.class)
